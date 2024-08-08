aa9e1ff32cb1cf1cafee086cad59466fd4420376
iDkGK <1444807655@qq.com>
2024-08-08 19:39:56 +0800

    Fix wave file playing issue on Linux.
    Signed-off-by: iDkGK <1444807655@qq.com>

--- audio.py
+++ audio.py
@@ -1,7 +1,7 @@
 import os
 import sys
-import time
 import tempfile
+import time
 import wave
 from concurrent.futures import Future, ProcessPoolExecutor
 from threading import Thread
@@ -12,17 +12,17 @@ def windows_wrapper(filepath: str, data: bytes, delay: float) -> None:
     from winsound import SND_MEMORY  # type: ignore
 
     time.sleep(delay)
-    winsound.PlaySound(data, SND_MEMORY)
+    winsound.PlaySound(data, SND_MEMORY)  # type: ignore
 
 
 def linux_wrapper(filepath: str, data: bytes, delay: float) -> None:
     time.sleep(delay)
-    os.popen("aplay %s" % filepath)
+    os.popen("aplay %s -q" % filepath)
 
 
 def macos_wrapper(filepath: str, data: bytes, delay: float) -> None:
     time.sleep(delay)
-    os.popen("afplay %s" % filepath)
+    os.popen("afplay %s -q 1" % filepath)
 
 
 if sys.platform.startswith("win"):
@@ -44,17 +44,19 @@ class WavePlayer(object):
         self._max_workers = max_workers
         self._pool_executor = ProcessPoolExecutor(max_workers=max_workers)
 
-    def play(self, filepath: str) -> None:
+    def _play_wav(self, filepath: str) -> None:
         with wave.open(filepath, "rb") as wav:
             wav_total_frames = wav.getnframes()
             wav_framerate = wav.getframerate()
             wav_params = wav.getparams()
-            per_part_time = wav_total_frames / wav_framerate / self._max_workers
+            wav_time = wav_total_frames / wav_framerate
+            per_part_time = wav_time / self._max_workers
             per_part_frames = wav_total_frames // self._max_workers
             last_part_frames = (
                 wav_total_frames - (self._max_workers - 1) * per_part_frames
             )
             async_pool_arguments: list[tuple[str, bytes, float]] = []
+            time_start = time.perf_counter()
             part_index = 0
             try:
                 for part_index in range(0, self._max_workers - 1):
@@ -87,7 +89,7 @@ class WavePlayer(object):
                         )
                 # Process pool
                 futures: list[Future[None]] = []
-                time_start = time.perf_counter()
+                async_time_start = time.perf_counter()
                 for part_filepath, part_data, part_delay in async_pool_arguments:
                     futures.append(
                         self._pool_executor.submit(
@@ -97,20 +99,24 @@ class WavePlayer(object):
                             (
                                 part_delay
                                 if part_delay == 0
-                                else part_delay - (time.perf_counter() - time_start)
+                                else part_delay
+                                - (time.perf_counter() - async_time_start)
                             ),
                         )
                     )
                 list(map(lambda future: future.result(), futures))
+                time.sleep(wav_time - (time.perf_counter() - time_start))
             finally:
                 for part_filepath, *_ in async_pool_arguments:
                     os.remove(part_filepath)
 
-    def async_play(self, filepath: str) -> None:
-        Thread(target=self.play, args=(filepath,)).start()
+    def play(self, filepath: str, asynchronous: bool = False) -> None:
+        if asynchronous:
+            Thread(target=self._play_wav, args=(filepath,), daemon=True).start()
+        else:
+            self._play_wav(filepath=filepath)
 
 
 if __name__ == "__main__":
     wave_player = WavePlayer()
-    target = wave_player.async_play("resource/audio/piano.wav")
-    target = wave_player.play("resource/audio/piano.wav")
+    target = wave_player.play("resource/audio/piano.wav", asynchronous=False)
